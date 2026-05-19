b2d8aa3e32e46b92bd2760f8ef26499581545bae
Vita <bondarenkovita35@gmail.com>
2026-05-19 14:34:07 +0200

    Added validation for review DTOs

--- ReviewService.Api/Dtos/CreateReviewDto.cs
+++ ReviewService.Api/Dtos/CreateReviewDto.cs
@@ -1,10 +1,19 @@
-﻿namespace ReviewService.Api.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReviewService.Api.Dtos;
+
+public class CreateReviewDto
 {
-    public class CreateReviewDto
-    {
-        public string UserName { get; set; } = string.Empty;
-        public string Comment { get; set; } = string.Empty;
-        public int Rating { get; set; }
-        public int CourseId { get; set; }
-    }
+    [Required]
+    public string UserName { get; set; } = string.Empty;
+
+    [Required]
+    [MinLength(3)]
+    public string Comment { get; set; } = string.Empty;
+
+    [Range(1, 5)]
+    public int Rating { get; set; }
+
+    [Range(1, int.MaxValue)]
+    public int CourseId { get; set; }
 }
